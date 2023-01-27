941ebc4f3814dd58206e0d7cb7f080ac846a339d
ClearCB <abelcasasccb@gmail.com>
2023-01-27 17:59:14 +0100

    finish spam

--- spamdominio/codigo.js
+++ spamdominio/codigo.js
@@ -1,9 +1,33 @@
-function consulta() {
-   let dominio = document.getElementById("dominio").value;
+var libres = 0;
+
+function consulta(dominio) {
    const xhttp = new XMLHttpRequest();
 
-   
+   xhttp.onload = function () {
+      if (this.responseText.length > 0) {
+         document.getElementById("information").innerHTML += "<br>"+dominio+" ocupado  ";
+      }
+      else {
+         document.getElementById("information").innerHTML += "<br>"+dominio+" libre  ";
+      }
+   }
 
    xhttp.open("GET", "https://rdap.verisign.com/com/v1/domain/" + dominio);
    xhttp.send();
+}
+function buscarDominios(){
+
+   const letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t']
+   
+   for (let i=0; i<10; i++){
+
+      for (let j=0; j<10; j++){
+         var domain = "a"+letters[i]+letters[j]+"ib.com";
+         consulta(domain);
+         }
+   }
+}
+
+function sumarLibres(){
+   libres+=1;
 }
