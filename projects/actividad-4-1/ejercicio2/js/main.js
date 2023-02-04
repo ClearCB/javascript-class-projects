47141df5c7d8885e08e10e41be3811e72bacebd7
Abel Casas <acasasgarcia1@cifpfbmoll.eu>
2023-02-04 13:28:39 +0100

    feat(actividad2): actividad 2 completada

--- projects/actividad-4-1/ejercicio2/js/main.js
+++ projects/actividad-4-1/ejercicio2/js/main.js
@@ -1,1 +1,28 @@
-alert("hola");
+function conseguirTextoNodo(nodoTag){
+
+    return document.getElementsByTagName(nodoTag);
+}
+
+function enseñarTextoNodo(nodos){
+
+    for (let i = 0; i<nodos.length; i++){
+
+        document.getElementById("segundo").innerHTML += nodos[i].textContent+"<br>";
+    }
+}
+
+// ...args es el equivalente a varargs (Java) o args* (python)
+function escribirTextos(...tiposNodo){
+
+    for (let tipoNodo of tiposNodo){
+
+        enseñarTextoNodo(conseguirTextoNodo(tipoNodo));
+    }
+
+}
+
+
+function main(){
+
+    escribirTextos("h3","p");
+}
